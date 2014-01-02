5eeb434b80dbde403094f4ee67203ecfb49ac3e8
Ernest Loveland <loveland.ernest@gmail.com>
2014-01-02 11:11:07 +0200

    Finished base for explosions.

--- SokoBomber/Assets/Scripts/Bomb/IgniteManager.cs
+++ SokoBomber/Assets/Scripts/Bomb/IgniteManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class IgniteManager : MonoBehaviour {
 	public bool IsIgnited = false;
 	public int Ticks = 2;
+	public GameObject ExplosionObject = null;
 
 	// Use this for initialization
 	void Start () {
@@ -50,8 +51,212 @@ public class IgniteManager : MonoBehaviour {
 			{
 				//Explode!
 				Debug.Log ("IgniteManager: TurnTick - Explode");
-				Destroy(this);
+
+				//FSM for each direction:
+				//UP! - increased Y
+				Vector3 startPoint = this.transform.position;
+				bool up_done = false;
+				while (!up_done)
+				{
+					Vector3 newPoint = startPoint + new Vector3(0,1,0);
+
+					var o_player = FindTaggedObjectAtPoint("Player", newPoint);
+					var o_coll = FindTaggedObjectAtPoint("Collidable", newPoint);
+					var o_destr = FindTaggedObjectAtPoint("Destructible", newPoint);
+					var o_movable = FindTaggedObjectAtPoint("Movable", newPoint);
+
+					if (o_player != null)
+					{
+						//player dies!
+						o_player.SendMessage("Die");
+					}
+					else if (o_coll != null)
+					{
+						//collidable means we stop!
+						up_done = true;
+					}
+					else if (o_destr != null)
+					{
+						//destructibles must die
+						Destroy (o_destr);
+						up_done = true;
+					}
+					else if (o_movable)
+					{
+						//Blow up the thing is it is a bomb
+						var cmpt = o_movable.GetComponentInChildren<IgniteManager>() as IgniteManager;
+						cmpt.IsIgnited = true;
+						if (cmpt.Ticks != 0)
+						{
+							cmpt.Ticks = 0;
+							cmpt.TurnTick();
+						}
+
+						up_done = true;
+					}
+
+					Instantiate(ExplosionObject, newPoint, Quaternion.identity);
+
+					startPoint = newPoint;
+				}
+
+				startPoint = this.transform.position;
+				bool down_done = false;
+				while (!down_done)
+				{
+					Vector3 newPoint = startPoint + new Vector3(0,-1,0);
+					
+					var o_player = FindTaggedObjectAtPoint("Player", newPoint);
+					var o_coll = FindTaggedObjectAtPoint("Collidable", newPoint);
+					var o_destr = FindTaggedObjectAtPoint("Destructible", newPoint);
+					var o_movable = FindTaggedObjectAtPoint("Movable", newPoint);
+					
+					if (o_player != null)
+					{
+						//player dies!
+						o_player.SendMessage("Die");
+					}
+					else if (o_coll != null)
+					{
+						//collidable means we stop!
+						down_done = true;
+					}
+					else if (o_destr != null)
+					{
+						//destructibles must die
+						Destroy (o_destr);
+						down_done = true;
+					}
+					else if (o_movable)
+					{
+						//Blow up the thing is it is a bomb
+						var cmpt = o_movable.GetComponentInChildren<IgniteManager>() as IgniteManager;
+						cmpt.IsIgnited = true;
+						if (cmpt.Ticks != 0)
+						{
+							cmpt.Ticks = 0;
+							cmpt.TurnTick();
+						}
+						
+						down_done = true;
+					}
+
+					Instantiate(ExplosionObject, newPoint, Quaternion.identity);
+					
+					startPoint = newPoint;
+				}
+
+				startPoint = this.transform.position;
+				bool left_done = false;
+				while (!left_done)
+				{
+					Vector3 newPoint = startPoint + new Vector3(-1,0,0);
+					
+					var o_player = FindTaggedObjectAtPoint("Player", newPoint);
+					var o_coll = FindTaggedObjectAtPoint("Collidable", newPoint);
+					var o_destr = FindTaggedObjectAtPoint("Destructible", newPoint);
+					var o_movable = FindTaggedObjectAtPoint("Movable", newPoint);
+					
+					if (o_player != null)
+					{
+						//player dies!
+						o_player.SendMessage("Die");
+					}
+					else if (o_coll != null)
+					{
+						//collidable means we stop!
+						left_done = true;
+					}
+					else if (o_destr != null)
+					{
+						//destructibles must die
+						Destroy (o_destr);
+						left_done = true;
+					}
+					else if (o_movable)
+					{
+						//Blow up the thing is it is a bomb
+						var cmpt = o_movable.GetComponentInChildren<IgniteManager>() as IgniteManager;
+						cmpt.IsIgnited = true;
+						if (cmpt.Ticks != 0)
+						{
+							cmpt.Ticks = 0;
+							cmpt.TurnTick();
+						}
+						
+						left_done = true;
+					}
+					
+					startPoint = newPoint;
+
+					Instantiate(ExplosionObject, newPoint, Quaternion.identity);
+				}
+
+				startPoint = this.transform.position;
+				bool right_down = false;
+				while (!right_down)
+				{
+					Vector3 newPoint = startPoint + new Vector3(1,0,0);
+					
+					var o_player = FindTaggedObjectAtPoint("Player", newPoint);
+					var o_coll = FindTaggedObjectAtPoint("Collidable", newPoint);
+					var o_destr = FindTaggedObjectAtPoint("Destructible", newPoint);
+					var o_movable = FindTaggedObjectAtPoint("Movable", newPoint);
+					
+					if (o_player != null)
+					{
+						//player dies!
+						o_player.SendMessage("Die");
+					}
+					else if (o_coll != null)
+					{
+						//collidable means we stop!
+						right_down = true;
+					}
+					else if (o_destr != null)
+					{
+						//destructibles must die
+						Destroy (o_destr);
+						right_down = true;
+					}
+					else if (o_movable)
+					{
+						//Blow up the thing is it is a bomb
+						var cmpt = o_movable.GetComponentInChildren<IgniteManager>() as IgniteManager;
+						cmpt.IsIgnited = true;
+						if (cmpt.Ticks != 0)
+						{
+							cmpt.Ticks = 0;
+							cmpt.TurnTick();
+						}
+						
+						right_down = true;
+					}
+
+					Instantiate(ExplosionObject, newPoint, Quaternion.identity);
+					
+					startPoint = newPoint;
+				}
+
+
+				Instantiate(ExplosionObject, this.transform.position, Quaternion.identity);
+
+				Destroy(gameObject);
 			}
 		}
+	}
+
+	private GameObject FindTaggedObjectAtPoint(string tag, Vector3 point)
+	{
+		var objs = GameObject.FindGameObjectsWithTag (tag);
+		for (int i = 0; i < objs.Length; i++)
+		{
+			if ((objs[i].transform.position - point).magnitude < 0.01f)
+			{
+				return objs[i];
+			}
+		}
+
+		return null;
 	}
 }
---
+++ SokoBomber/Assets/Scripts/ExplosionAnimate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionAnimate : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	float timeDone = 0f;
+	void Update () {
+		timeDone += Time.deltaTime;
+		if (timeDone >= 0.9f)
+		{
+			Destroy (this.gameObject);
+		}
+	}
+	
+}
