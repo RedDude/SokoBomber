035aa7883a492eafc104b45b619e902988b51254
Ernest Loveland <loveland.ernest@gmail.com>
2014-01-10 09:54:59 +0200

    Wall Destruction

---
+++ SokoBomber/Assets/Scripts/OnDestroySpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnDestroySpawner : MonoBehaviour {
+    public GameObject ToSpawn = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDestroy()
+    {
+        Instantiate(ToSpawn, this.transform.position, Quaternion.identity);
+    }
+}
