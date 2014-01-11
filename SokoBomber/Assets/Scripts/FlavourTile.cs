6e04bcbabad9d6bf49809ba775a657739656ad5d
Paul Lombard <pmlomb@gmail.com>
2014-01-11 10:57:01 +0200

    added flavour tiles + flavour script to walls and floors to random tile on start.

---
+++ SokoBomber/Assets/Scripts/FlavourTile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlavourTile : MonoBehaviour
+{
+
+    public Sprite[] flavourTiles;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    var index = Random.Range(0, flavourTiles.Length);
+	    GetComponent<SpriteRenderer>().sprite = flavourTiles[index];
+
+	}
+	
+
+}
