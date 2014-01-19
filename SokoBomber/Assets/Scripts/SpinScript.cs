7054f4f791c11700a1d65a657e0a313679f4fe38
Paul Lombard <pmlomb@gmail.com>
2014-01-19 16:25:15 +0200

    Blast Particles Ice tiles and walls Main menu sound Ingame hud buttons menu quit reset and backing debrief cleanup and particle juicing level choosing cleanup

---
+++ SokoBomber/Assets/Scripts/SpinScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinScript : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    transform.Rotate(Vector3.forward*speed*Time.deltaTime);
+
+	}
+}
